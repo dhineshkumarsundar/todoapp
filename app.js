0a8ee74c5a596c447029d5ced15b2a5873df8cbd
DhineshKumarSundar <41160069+dhineshkumarsundar@users.noreply.github.com>
2019-06-19 07:45:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 var item = document.getElementsByTagName("li");
 
-function inputLength(){
-	return input.value.length;
-} 
-
-function listLength(){
-	return item.length;
-}
 
 function createListElement() {
     var li = document.createElement("li");
